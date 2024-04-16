5cdb892b3452c48ca598d7b72859b4bb0ee56d72
Börcsök Balázs Róbert <pauljouser@protonmail.com>
2024-04-17 00:07:45 +0200

    Deleted unneded files, UI shows now.

--- app/src/main/java/com/iplayzed/cuttingstockoptimizer/MainActivity.kt
+++ app/src/main/java/com/iplayzed/cuttingstockoptimizer/MainActivity.kt
@@ -1,52 +1,19 @@
 package com.iplayzed.cuttingstockoptimizer
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.iplayzed.cuttingstockoptimizer.ui.theme.CuttingStockOptimizerTheme
+import androidx.appcompat.app.AppCompatActivity
 import org.ojalgo.optimisation.ExpressionsBasedModel
 import org.ojalgo.optimisation.Variable
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CuttingStockOptimizerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_wizard_step_one);
     }
+    // TODO: Handle UI IO.
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CuttingStockOptimizerTheme {
-        Greeting("Android")
-    }
-}
-
+// TODO: Use inputs dynamically.
 private fun performOptimization(){
     val model = ExpressionsBasedModel()
 
