480035507053517c255676b797b497348301e81d
Börcsök Balázs Róbert <pauljouser@protonmail.com>
2024-04-28 19:34:58 +0200

    LP finds a solution, but it is not good, idk why.

--- app/src/main/java/com/iplayzed/cuttingstockoptimizer/MainActivity.kt
+++ app/src/main/java/com/iplayzed/cuttingstockoptimizer/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.ojalgo.optimisation.Expression
 import org.ojalgo.optimisation.ExpressionsBasedModel
 import org.ojalgo.optimisation.Variable
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +17,6 @@ class MainActivity : AppCompatActivity() {
         val stoneBlockWidthTextView: TextView = findViewById(R.id.stoneBlockWidth)
         val blockLength1TextView: TextView = findViewById(R.id.blockLength1)
         val blockLength2TextView: TextView = findViewById(R.id.blockLength2)
-        val transitionTimeTextView: TextView = findViewById(R.id.transitionTime)
         val generateTableButton: Button = findViewById(R.id.generateTable)
 
         val widthHorizontalTextView: TextView = findViewById(R.id.widthHorizontal)
@@ -31,7 +32,6 @@ class MainActivity : AppCompatActivity() {
             val stoneBlockWidth = stoneBlockWidthTextView.getText().toString().toInt()
             val blockLength1 = blockLength1TextView.getText().toString().toInt()
             val blockLength2 = blockLength2TextView.getText().toString().toInt()
-            //val transitionTime = transitionTimeTextView.getText().toString().toInt()
 
             widthHorizontalTextView.text = stoneBlockWidth.toString()
             widthVerticalTextView.text = stoneBlockWidth.toString()
@@ -82,114 +82,110 @@ private fun performOptimization(
 
     // Comments are for concrete 40w,60/80h.
 
-    val X_W__W_W = model.newVariable("X_W__W_W").integer().lower(0) //X444
-    val X_W__W_H1 = model.newVariable("X_W__W_H1").integer().lower(0) //X446
-    val X_W__W_H2 = model.newVariable("X_W__W_H2").integer().lower(0) //X448
-    val X_W__H1_H1 = model.newVariable("X_W__H1_H1").integer().lower(0) //X466
-    val X_W__H1_H2 = model.newVariable("X_W__H1_H2").integer().lower(0) //X468
-    val X_W__H2_H2 = model.newVariable("X_W__H2_H2").integer().lower(0) //X488
-    val X_H1__W_W = model.newVariable("X_H1__W_W").integer().lower(0) //X644
-    val X_H1__W_H1 = model.newVariable("X_H1__W_H1").integer().lower(0) //X646
-    val X_H1__W_H2 = model.newVariable("X_H1__W_H2").integer().lower(0) //X648
-    val X_H1__H1_H1 = model.newVariable("X_H1__H1_H1").integer().lower(0) //X666
-    val X_H1__H1_H2 = model.newVariable("X_H1__H1_H2").integer().lower(0) //X668
-    val X_H1__H2_H2 = model.newVariable("X_H1__H2_H2").integer().lower(0) //X688
-    val X_H2__W_W = model.newVariable("X_H2__W_W").integer().lower(0) //X844
-    val X_H2__W_H1 = model.newVariable("X_H2__W_H1").integer().lower(0) //X846
-    val X_H2__W_H2 = model.newVariable("X_H2__W_H2").integer().lower(0) //X848
-    val X_H2__H1_H1 = model.newVariable("X_H2__H1_H1").integer().lower(0) //X866
-    val X_H2__H1_H2 = model.newVariable("X_H2__H1_H2").integer().lower(0) //X868
-    val X_H2__H2_H2 = model.newVariable("X_H2__H2_H2").integer().lower(0) //X888
+    val X_W__W_W = model.addVariable("X_W__W_W").weight(1).lower(0).integer() //X444
+    val X_W__W_H1 = model.addVariable("X_W__W_H1").weight(1).lower(0).integer()  //X446
+    val X_W__W_H2 = model.addVariable("X_W__W_H2").weight(1).lower(0).integer() //X448
+    val X_W__H1_H1 = model.addVariable("X_W__H1_H1").weight(1).lower(0).integer() //X466
+    val X_W__H1_H2 = model.addVariable("X_W__H1_H2").weight(1).lower(0).integer() //X468
+    val X_W__H2_H2 = model.addVariable("X_W__H2_H2").weight(1).lower(0).integer() //X488
+    val X_H1__W_W = model.addVariable("X_H1__W_W").weight(1).lower(0).integer() //X644
+    val X_H1__W_H1 = model.addVariable("X_H1__W_H1").weight(1).lower(0).integer() //X646
+    val X_H1__W_H2 = model.addVariable("X_H1__W_H2").weight(1).lower(0).integer() //X648
+    val X_H1__H1_H1 = model.addVariable("X_H1__H1_H1").weight(1).lower(0).integer() //X666
+    val X_H1__H1_H2 = model.addVariable("X_H1__H1_H2").weight(1).lower(0).integer() //X668
+    val X_H1__H2_H2 = model.addVariable("X_H1__H2_H2").weight(1).lower(0).integer() //X688
+    val X_H2__W_W = model.addVariable("X_H2__W_W").weight(1).lower(0).integer() //X844
+    val X_H2__W_H1 = model.addVariable("X_H2__W_H1").weight(1).lower(0).integer() //X846
+    val X_H2__W_H2 = model.addVariable("X_H2__W_H2").weight(1).lower(0).integer() //X848
+    val X_H2__H1_H1 = model.addVariable("X_H2__H1_H1").weight(1).lower(0).integer() //X866
+    val X_H2__H1_H2 = model.addVariable("X_H2__H1_H2").weight(1).lower(0).integer() //X868
+    val X_H2__H2_H2 = model.addVariable("X_H2__H2_H2").weight(1).lower(0).integer() //X888
 
-    model.newVariable("Y_W__W_W").binary() //Y444
-    model.newVariable("Y_W__W_H1").binary() //Y446
-    model.newVariable("Y_W__W_H2").binary() //Y448
-    model.newVariable("Y_W__H1_H1").binary() //Y466
-    model.newVariable("Y_W__H1_H2").binary() //Y468
-    model.newVariable("Y_W__H2_H2").binary() //Y488
-    model.newVariable("Y_H1__W_W").binary() //Y644
-    model.newVariable("Y_H1__W_H1").binary() //Y646
-    model.newVariable("Y_H1__W_H2").binary() //Y648
-    model.newVariable("Y_H1__H1_H1").binary() //Y666
-    model.newVariable("Y_H1__H1_H2").binary() //Y668
-    model.newVariable("Y_H1__H2_H2").binary() //Y688
-    model.newVariable("Y_H2__W_W").binary() //Y844
-    model.newVariable("Y_H2__W_H1").binary() //Y846
-    model.newVariable("Y_H2__W_H2").binary() //Y848
-    model.newVariable("Y_H2__H1_H1").binary() //Y866
-    model.newVariable("Y_H2__H1_H2").binary() //Y868
-    model.newVariable("Y_H2__H2_H2").binary() //Y888
+    val Y_W__W_W = model.addVariable("Y_W__W_W").binary().weight(1) //Y444
+    val Y_W__W_H1 = model.addVariable("Y_W__W_H1").binary().weight(1) //Y446
+    val Y_W__W_H2 = model.addVariable("Y_W__W_H2").binary().weight(1) //Y448
+    val Y_W__H1_H1 = model.addVariable("Y_W__H1_H1").binary().weight(1) //Y466
+    val Y_W__H1_H2 = model.addVariable("Y_W__H1_H2").binary().weight(1) //Y468
+    val Y_W__H2_H2 = model.addVariable("Y_W__H2_H2").binary().weight(1) //Y488
+    val Y_H1__W_W = model.addVariable("Y_H1__W_W").binary().weight(1) //Y644
+    val Y_H1__W_H1 = model.addVariable("Y_H1__W_H1").binary().weight(1) //Y646
+    val Y_H1__W_H2 = model.addVariable("Y_H1__W_H2").binary().weight(1) //Y648
+    val Y_H1__H1_H1 = model.addVariable("Y_H1__H1_H1").binary().weight(1) //Y666
+    val Y_H1__H1_H2 = model.addVariable("Y_H1__H1_H2").binary().weight(1) //Y668
+    val Y_H1__H2_H2 = model.addVariable("Y_H1__H2_H2").binary().weight(1) //Y688
+    val Y_H2__W_W = model.addVariable("Y_H2__W_W").binary().weight(1) //Y844
+    val Y_H2__W_H1 = model.addVariable("Y_H2__W_H1").binary().weight(1) //Y846
+    val Y_H2__W_H2 = model.addVariable("Y_H2__W_H2").binary().weight(1) //Y848
+    val Y_H2__H1_H1 = model.addVariable("Y_H2__H1_H1").binary().weight(1) //Y866
+    val Y_H2__H1_H2 = model.addVariable("Y_H2__H1_H2").binary().weight(1) //Y868
+    val Y_H2__H2_H2 = model.addVariable("Y_H2__H2_H2").binary().weight(1) //Y888
 
     // Constraints
-    model.addExpression("A__W_W").upper(cell11) //A44
+    model.addExpression("A__W_W") //A44
+        .lower(cell11)
         .set(X_W__W_W, 2) //X444
         .set(X_W__W_H1, 1) //X446
         .set(X_W__W_H2, 1) //X448
-    model.addExpression("A__W_H1").upper(cell12) //A46
+        .weight(1)
+    model.addExpression("A__W_H1") //A46
+        .lower(cell12)
         .set(X_W__W_H1, 1) //X446
         .set(X_W__H1_H1, 2) //X466
         .set(X_W__H1_H2, 1) //X468
         .set(X_H1__W_W, 2) //X644
         .set(X_H1__W_H1, 1) //X646
         .set(X_H1__W_H2, 1) //X648
-    model.addExpression("A__W_H2").upper(cell13) //A48
+        .weight(1)
+    model.addExpression("A__W_H2") //A48
+        .lower(cell13)
         .set(X_W__W_H2, 1) //X448
         .set(X_W__H1_H2, 1) //X468
         .set(X_W__H2_H2, 2) //X488
         .set(X_H2__W_W, 2) //X844
         .set(X_H2__W_H1, 1) //X846
         .set(X_H2__W_H2, 1) //X848
-    model.addExpression("A__H1_H1").upper(cell22) //A66
+        .weight(1)
+    model.addExpression("A__H1_H1") //A66
+        .lower(cell22)
         .set(X_H1__W_H1, 1) //X646
         .set(X_H1__H1_H1, 2) //X666
         .set(X_H1__H1_H2, 1) //X668
-    model.addExpression("A__H1_H2").upper(cell23) //A68
+        .weight(1)
+    model.addExpression("A__H1_H2") //A68
+        .lower(cell23)
         .set(X_H1__W_H2, 1) //X648
         .set(X_H1__H1_H2, 1) //X668
         .set(X_H1__H2_H2, 2) //X688
         .set(X_H2__W_H1, 1) //X846
         .set(X_H2__H1_H1, 2) //X866
         .set(X_H2__H1_H2, 1) //X868
-    model.addExpression("A__H2_H2").upper(cell33) //A88
+        .weight(1)
+    model.addExpression("A__H2_H2") //A88
+        .lower(cell33)
         .set(X_H2__W_H2, 1) //X848
         .set(X_H2__H1_H2, 1) //X868
         .set(X_H2__H2_H2, 2) //X888
+        .weight(1)
 
-    return model.minimise().value;
-}
+    model.addExpression().weight(1).set(X_W__W_W, 1).set(Y_W__W_W, -100).upper(0)
+    model.addExpression().weight(1).set(X_W__W_H1, 1).set(Y_W__W_H1, -100).upper(0)
+    model.addExpression().weight(1).set(X_W__W_H2, 1).set(Y_W__W_H2, -100).upper(0)
+    model.addExpression().weight(1).set(X_W__H1_H1, 1).set(Y_W__H1_H1, -100).upper(0)
+    model.addExpression().weight(1).set(X_W__H1_H2, 1).set(Y_W__H1_H2, -100).upper(0)
+    model.addExpression().weight(1).set(X_W__H2_H2, 1).set(Y_W__H2_H2, -100).upper(0)
+    model.addExpression().weight(1).set(X_H1__W_W, 1).set(Y_H1__W_W, -100).upper(0)
+    model.addExpression().weight(1).set(X_H1__W_H1, 1).set(Y_H1__W_H1, -100).upper(0)
+    model.addExpression().weight(1).set(X_H1__W_H2, 1).set(Y_H1__W_H2, -100).upper(0)
+    model.addExpression().weight(1).set(X_H1__H1_H1, 1).set(Y_H1__H1_H1, -100).upper(0)
+    model.addExpression().weight(1).set(X_H1__H1_H2, 1).set(Y_H1__H1_H2, -100).upper(0)
+    model.addExpression().weight(1).set(X_H1__H2_H2, 1).set(Y_H1__H2_H2, -100).upper(0)
+    model.addExpression().weight(1).set(X_H2__W_W, 1).set(Y_H2__W_W, -100).upper(0)
+    model.addExpression().weight(1).set(X_H2__W_H1, 1).set(Y_H2__W_H1, -100).upper(0)
+    model.addExpression().weight(1).set(X_H2__W_H2, 1).set(Y_H2__W_H2, -100).upper(0)
+    model.addExpression().weight(1).set(X_H2__H1_H1, 1).set(Y_H2__H1_H1, -100).upper(0)
+    model.addExpression().weight(1).set(X_H2__H1_H2, 1).set(Y_H2__H1_H2, -100).upper(0)
+    model.addExpression().weight(1).set(X_H2__H2_H2, 1).set(Y_H2__H2_H2, -100).upper(0)
 
-// TODO: Use inputs dynamically.
-private fun performOptimization2(): Double {
-    val model = ExpressionsBasedModel()
-
-    // Felhasználó által megadott adatok
-    val products = listOf("40x40", "40x60", "60x60", "60x80", "80x80")
-    val requirements = mapOf("40x40" to 10, "40x60" to 20, "60x60" to 5, "60x80" to 10, "80x80" to 20)
-    val variables = mutableMapOf<String, Variable>()
-    val binaryVariables = mutableMapOf<String, Variable>()
-
-    // Változók létrehozása
-    for (product in products) {
-        val x = Variable.make("x_$product").lower(0).integer(true).weight(1)
-        val y = Variable.make("y_$product").binary().weight(1)
-        model.addVariable(x)
-        model.addVariable(y)
-        variables[product] = x
-        binaryVariables[product] = y
-    }
-
-    // Korlátok beállítása
-    for ((product, requirement) in requirements) {
-        val expression = model.addExpression("Require_$product").lower(requirement.toDouble())
-        expression.set(variables[product], 1)
-        // Kapcsolat az x és y változók között
-        val linkExpression = model.addExpression("Link_$product").upper(0)
-        linkExpression.set(variables[product], 1)
-        linkExpression.set(binaryVariables[product], -100)
-    }
-
-    // Célfüggvény: Minimális gyártási és átállási napok
-    val result = model.minimise()
-
+    val result = model.maximise()
     return result.value
 }
